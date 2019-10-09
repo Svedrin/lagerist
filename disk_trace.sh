9d36a6e9147f86f038c50b5de37628980ba00a62
Michael Ziegler <michael.ziegler@proemion.com>
2019-10-09 19:41:34 +0200

    add disk_trace.sh and example output

---
+++ disk_trace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -u
+
+BASE=/sys/kernel/debug/tracing/instances
+INST="$BASE/lagerist"
+
+cleanup() {
+    echo 0 > "$INST/tracing_on"
+    rmdir "$INST"
+}
+
+trap cleanup exit
+
+mkdir -p "$INST"
+
+echo 1 > "$INST/events/block/block_rq_issue/enable"
+echo 1 > "$INST/events/block/block_rq_insert/enable"
+echo 1 > "$INST/events/block/block_rq_complete/enable"
+
+echo 1 > "$INST/tracing_on"
+
+cat "$INST/trace_pipe"
