1a1c68a89db33d2a92b96325d2a8a93c24febb8e
Michael Ziegler <michael.ziegler@proemion.com>
2019-10-10 00:09:29 +0200

    also let's indent this shit

--- disk_trace.sh
+++ disk_trace.sh
@@ -14,11 +14,9 @@ cleanup() {
 trap cleanup exit
 
 mkdir -p "$INST"
-
 echo 1 > "$INST/events/block/block_rq_issue/enable"
 echo 1 > "$INST/events/block/block_rq_insert/enable"
 echo 1 > "$INST/events/block/block_rq_complete/enable"
-
 echo 1 > "$INST/tracing_on"
 
 cat "$INST/trace_pipe"
